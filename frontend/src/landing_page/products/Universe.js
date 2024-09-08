3d67bad39b7780393d4ae5018c259d727144c57c
Vivek Kumar <vivek70489@gmail.com>
2024-09-08 10:40:19 +0530

    Added Files

---
+++ frontend/src/landing_page/products/Universe.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function Universe() {
+    return (
+        <div className="container mt-5">
+            <div className="row text-center">
+                <h1>The Zerodha Universe</h1>
+                <p>Extend your trading and investment experience even further with our partner platforms</p>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media/images/smallcaseLogo.png" />
+                    <p className="text-small text-muted">Our asset management venture
+                        that is creating simple and transparent index
+                        funds to help you save for your goals.</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media/images/smallcaseLogo.png" />
+                    <p className="text-small text-muted">Our asset management venture
+                        that is creating simple and transparent index
+                        funds to help you save for your goals.</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media/images/smallcaseLogo.png" />
+                    <p className="text-small text-muted">Our asset management venture
+                        that is creating simple and transparent index
+                        funds to help you save for your goals.</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media/images/smallcaseLogo.png" />
+                    <p className="text-small text-muted">Our asset management venture
+                        that is creating simple and transparent index
+                        funds to help you save for your goals.</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media/images/smallcaseLogo.png" />
+                    <p className="text-small text-muted">Our asset management venture
+                        that is creating simple and transparent index
+                        funds to help you save for your goals.</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media/images/smallcaseLogo.png" />
+                    <p className="text-small text-muted">Our asset management venture
+                        that is creating simple and transparent index
+                        funds to help you save for your goals.</p>
+                </div>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{ width: "20%", margin: "0 auto" }}>Signup Now</button>
+            </div>
+        </div>
+    );
+}
+
+export default Universe;
---
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+function CreateTicket() {
+    return (
+        <div className="container">
+            <div className="row p-5 mt-5 mb-5">
+                <h1 className="fs-2">
+                    To create a ticket, select a relevant topic
+                </h1>
+                <div className="col-4 p-5 mt-2 mb-2">
+                    <h4 className="">
+                        <i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening
+                    </h4>
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Getting started</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Online</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Offline</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Charges</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Company, Partnership and HUF</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Non Resident Indian (NRI)</a> <br />
+                </div>
+                <div className="col-4 p-5 mt-2 mb-2">
+                    <h4 className="">
+                        <i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening
+                    </h4>
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Getting started</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Online</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Offline</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Charges</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Company, Partnership and HUF</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Non Resident Indian (NRI)</a> <br />
+                </div>
+                <div className="col-4 p-5 mt-2 mb-2">
+                    <h4 className="">
+                        <i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening
+                    </h4>
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Getting started</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Online</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Offline</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Charges</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Company, Partnership and HUF</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Non Resident Indian (NRI)</a> <br />
+                </div>
+                <div className="col-4 p-5 mt-2 mb-2">
+                    <h4 className="">
+                        <i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening
+                    </h4>
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Getting started</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Online</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Offline</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Charges</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Company, Partnership and HUF</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Non Resident Indian (NRI)</a> <br />
+                </div>
+                <div className="col-4 p-5 mt-2 mb-2">
+                    <h4 className="">
+                        <i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening
+                    </h4>
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Getting started</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Online</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Offline</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Charges</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Company, Partnership and HUF</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Non Resident Indian (NRI)</a> <br />
+                </div>
+                <div className="col-4 p-5 mt-2 mb-2">
+                    <h4 className="">
+                        <i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening
+                    </h4>
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Getting started</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Online</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Offline</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Charges</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Company, Partnership and HUF</a> <br />
+                    <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>Non Resident Indian (NRI)</a> <br />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CreateTicket;
